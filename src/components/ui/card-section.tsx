1fff196a4bab3b9b61169bb224baf24fc67b4f89
LinCie <aldiofernanda@protonmail.com>
2024-11-26 23:53:25 +0700

    feat: add card section component

---
+++ src/components/ui/card-section.tsx
@@ -0,0 +1,88 @@
+import { cn } from "@/lib/utils";
+import React from "react";
+import { Card } from "./card";
+
+function CardSection({
+  className,
+  ...props
+}: React.ComponentPropsWithRef<"section">) {
+  return (
+    <section
+      className={cn("w-full px-4 py-10 md:py-16 lg:py-20", className)}
+      {...props}
+    />
+  );
+}
+
+function CardSectionContainer({
+  className,
+  ...props
+}: React.ComponentPropsWithRef<"div">) {
+  return (
+    <Card
+      className={cn(
+        "mx-auto max-w-screen-xl px-6 backdrop-blur md:px-8 lg:px-12",
+        className,
+      )}
+      {...props}
+    />
+  );
+}
+
+function CardSectionHeader({
+  className,
+  ...props
+}: React.ComponentPropsWithRef<"header">) {
+  return (
+    <header
+      className={cn("flex flex-col space-y-1.5 px-3 pt-12", className)}
+      {...props}
+    />
+  );
+}
+
+function CardSectionTitle({
+  className,
+  ...props
+}: React.ComponentPropsWithRef<"h2">) {
+  return (
+    <h2
+      className={cn(
+        "max-w-xl text-3xl font-bold leading-tight text-primary lg:text-4xl",
+        className,
+      )}
+      {...props}
+    />
+  );
+}
+
+function CardSectionSubtitle({
+  className,
+  ...props
+}: React.ComponentPropsWithRef<"p">) {
+  return (
+    <p
+      className={cn(
+        "max-w-2xl pb-6 text-base text-muted-foreground md:text-lg",
+        className,
+      )}
+      {...props}
+    />
+  );
+}
+
+function CardSectionContent({
+  className,
+  ...props
+}: React.ComponentPropsWithRef<"div">) {
+  return <div className={cn("px-3 pb-12 md:px-6", className)} {...props} />;
+}
+
+export {
+  CardSection,
+  CardSectionContainer,
+  CardSectionHeader,
+  CardSectionTitle,
+  CardSectionSubtitle,
+  CardSectionContent,
+};
