0b1bd0feb34d965ccaa1e278112a2b074ac7b149
LinCie <aldiofernanda@protonmail.com>
2024-12-13 23:47:40 +0700

    perf: remove unnecessary blurfade

--- src/views/landing/sections/product.tsx
+++ src/views/landing/sections/product.tsx
@@ -41,74 +41,90 @@ function ProductCard({
 }: ProductCardProps) {
   const detectMobile = useMobileDetect();
 
+  const isInInitialView = detectMobile.isMobile() ? index < 1 : index < 4;
+
+  function ProductContent() {
+    return (
+      <Card className="group/card flex h-fit flex-col overflow-hidden rounded-xl">
+        <div className="relative h-[300px] select-none overflow-hidden">
+          <Image
+            loading="lazy"
+            decoding="async"
+            height={300}
+            width={400}
+            src={image}
+            alt={name}
+            className="h-full w-full object-cover object-center transition group-hover/card:scale-105 group-hover/card:blur-[2px]"
+          />
+          <div className="absolute inset-0 flex items-center justify-center transition group-hover/card:bg-black/50">
+            <Button
+              variant="expandIcon"
+              iconPlacement="right"
+              Icon={LuExternalLink}
+              size="sm"
+              className="bg-transparent text-background opacity-0 outline outline-1 outline-background transition-opacity hover:bg-background hover:text-foreground group-hover/card:opacity-100"
+            >
+              Lihat di Katalog
+            </Button>
+          </div>
+        </div>
+        <div className="from-neutral-100 to-transparent p-2 transition-colors group-hover/card:bg-gradient-to-br">
+          <div className="font-semibold transition-all group-hover/card:translate-x-2">
+            {name}
+          </div>
+          <div className="mb-4 truncate text-sm">{description}</div>
+          <div className="select-none space-y-2">
+            <Button
+              variant="ringHover"
+              size="sm"
+              className="group/button w-full"
+              asChild
+            >
+              <Link target="_blank" href={link}>
+                Lihat di Katalog{" "}
+                <LuStore
+                  aria-hidden
+                  className="ml-2 size-4 transition-all group-hover/button:translate-x-1 group-hover/button:rotate-6"
+                />
+              </Link>
+            </Button>
+            <Button
+              variant="ringHover"
+              size="sm"
+              className="group/button w-full bg-green-500 hover:bg-green-500/90 hover:ring-green-500/90"
+              asChild
+            >
+              <Link target="_blank" href={link}>
+                Chat Kami{" "}
+                <FaWhatsapp
+                  aria-hidden
+                  className="ml-2 size-4 transition-all group-hover/button:translate-x-1 group-hover/button:rotate-6"
+                />
+              </Link>
+            </Button>
+          </div>
+        </div>
+      </Card>
+    );
+  }
+
+  if (isInInitialView) {
+    return (
+      <CarouselItem className="pl-8 sm:basis-1/2 md:basis-1/3 lg:basis-1/4">
+        <BlurFade
+          inView
+          inViewMargin="-100px"
+          delay={detectMobile.isMobile() ? 0 : 0.2 * index}
+        >
+          <ProductContent />
+        </BlurFade>
+      </CarouselItem>
+    );
+  }
+
   return (
     <CarouselItem className="pl-8 sm:basis-1/2 md:basis-1/3 lg:basis-1/4">
-      <BlurFade
-        inView={detectMobile.isMobile() ? index < 1 : index < 4}
-        inViewMargin="-100px"
-        delay={detectMobile.isMobile() ? 0 : 0.2 * index}
-      >
-        <Card className="group/card flex h-fit flex-col overflow-hidden rounded-xl">
-          <div className="relative h-[300px] select-none overflow-hidden">
-            <Image
-              loading="lazy"
-              decoding="async"
-              height={300}
-              width={400}
-              src={image}
-              alt={name}
-              className="h-full w-full object-cover object-center transition group-hover/card:scale-105 group-hover/card:blur-[2px]"
-            />
-            <div className="absolute inset-0 flex items-center justify-center transition group-hover/card:bg-black/50">
-              <Button
-                variant="expandIcon"
-                iconPlacement="right"
-                Icon={LuExternalLink}
-                size="sm"
-                className="bg-transparent text-background opacity-0 outline outline-1 outline-background transition-opacity hover:bg-background hover:text-foreground group-hover/card:opacity-100"
-              >
-                Lihat di Katalog
-              </Button>
-            </div>
-          </div>
-          <div className="from-neutral-100 to-transparent p-2 transition-colors group-hover/card:bg-gradient-to-br">
-            <div className="font-semibold transition-all group-hover/card:translate-x-2">
-              {name}
-            </div>
-            <div className="mb-4 truncate text-sm">{description}</div>
-            <div className="select-none space-y-2">
-              <Button
-                variant="ringHover"
-                size="sm"
-                className="group/button w-full"
-                asChild
-              >
-                <Link target="_blank" href={link}>
-                  Lihat di Katalog{" "}
-                  <LuStore
-                    aria-hidden
-                    className="ml-2 size-4 transition-all group-hover/button:translate-x-1 group-hover/button:rotate-6"
-                  />
-                </Link>
-              </Button>
-              <Button
-                variant="ringHover"
-                size="sm"
-                className="group/button w-full bg-green-500 hover:bg-green-500/90 hover:ring-green-500/90"
-                asChild
-              >
-                <Link target="_blank" href={link}>
-                  Chat Kami{" "}
-                  <FaWhatsapp
-                    aria-hidden
-                    className="ml-2 size-4 transition-all group-hover/button:translate-x-1 group-hover/button:rotate-6"
-                  />
-                </Link>
-              </Button>
-            </div>
-          </div>
-        </Card>
-      </BlurFade>
+      <ProductContent />
     </CarouselItem>
   );
 }
