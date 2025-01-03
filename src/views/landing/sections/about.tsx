a04437d6b22fcd623bdef0cd3d8d0206a5d59bd8
LinCie <aldiofernanda@protonmail.com>
2025-01-03 18:40:50 +0700

    style: adjust about image

--- src/views/landing/sections/about.tsx
+++ src/views/landing/sections/about.tsx
@@ -5,7 +5,7 @@ import {
   CardSectionTitle,
 } from "@/components/ui/card-section";
 import Image from "next/image";
-import machineImage from "@/assets/images/machine.jpg";
+import aboutImage from "@/assets/images/customer-1.webp";
 import BlurFade from "@/components/ui/blur-fade";
 
 export default function AboutSection() {
@@ -13,7 +13,7 @@ export default function AboutSection() {
     <CardSection id="about-section" aria-labelledby="about-section-title">
       <CardSectionContainer className="flex flex-col overflow-hidden px-0 pl-0 md:px-0 md:pl-0 lg:flex-row lg:px-0 lg:pl-0">
         <Image
-          src={machineImage}
+          src={aboutImage}
           alt="haebot store"
           className="h-[300px] w-full object-cover lg:h-[500px] lg:max-w-lg"
           decoding="async"
