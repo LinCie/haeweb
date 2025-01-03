0eb9d8e479b979a2406d8f836ebe0391f02cc82d
LinCie <aldiofernanda@protonmail.com>
2025-01-03 19:14:11 +0700

    feat: add haebot icon

--- src/components/layout/top-header.tsx
+++ src/components/layout/top-header.tsx
@@ -1,9 +1,14 @@
+import Haebot from "@/assets/haebot";
+
 export default function TopHeader() {
   return (
     <div className="z-20 flex h-20 items-center justify-center bg-background px-6 md:px-12">
-      <h1 className="max-w-40 text-base font-medium tracking-tight md:max-w-xs md:text-lg lg:text-xl">
-        PT Haebot Teknologi Indonesia
-      </h1>
+      <div className="flex items-center justify-center">
+        <Haebot className="size-20 fill-primary" />
+        <h1 className="max-w-40 text-base font-medium leading-3 tracking-tight md:max-w-xs md:text-lg lg:text-xl">
+          PT Haebot Teknologi Indonesia
+        </h1>
+      </div>
       <div className="flex flex-1 flex-col items-end justify-center text-sm md:text-base">
         <div className="font-medium text-primary">Konsultasi Gratis</div>
         <div>(+62) 852-4642-8746</div>
